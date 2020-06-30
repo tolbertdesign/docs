fbc66500b705bbafb5afa9f3edaa8fec8b07b538
Victor Tolbert <victor.tolbert@gmail.com>
2020-06-30 13:29:59 -0400

    chore: add @nuxtjs/tailwindcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   components: true,
   modules: ['@nuxt/content'],
+  buildModules: ['@nuxtjs/tailwindcss'],
   target: 'static',
   generate: {
     routes: ['/'], // the first url to start crawling

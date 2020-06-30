7b8c430f96c49fa388d9e2455bdc5def6f5c0dfd
@design_coder <62181278+tolbertdesign@users.noreply.github.com>
2020-06-30 11:49:06 -0400

    chore: add @nuxt/content module (#1)
    Write in a content/ directory and fetch Markdown, JSON, YAML, XML and CSV files through a MongoDB like API, acting as a Git-based Headless CMS.
    
    Co-authored-by: Victor Tolbert <victor.tolbert@gmail.com>

---
+++ pages/_slug.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <article>
+        <h1>{{ doc.title }}</h1>
+
+        <NuxtContent :document="doc" />
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageSlug",
+  middleware({ params, redirect }) {
+    if (params.slug === "index") {
+      redirect("/");
+    }
+  },
+  async asyncData({ $content, store, app, params, error }) {
+    const slug = params.slug || "index";
+
+    let doc;
+    try {
+      doc = await $content(slug).fetch();
+    } catch (e) {
+      return error({ statusCode: 404, message: "Page not found" });
+    }
+
+    return { doc };
+  },
+};
+</script>
